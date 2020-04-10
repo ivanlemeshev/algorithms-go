e332316550990b7cbac1e9ca9487bf8ab1da9bd7
Ivan Lemeshev <vanlmshv@gmail.com>
2020-04-11 01:38:50 +0300

    Add QuickUnion

---
+++ unionfind/quickunion/quickunion.go
@@ -0,0 +1,103 @@
+package quickunion
+
+import (
+	"fmt"
+
+	"github.com/ivanlemeshev/algorithms-go/unionfind"
+)
+
+// QuickFind is an implementation of union–find data type. This implementation
+// uses quick union. The constructor takes O(n) time, where n is the number of
+// sites. The union and find operations take O(n) time in the worst case. The
+// count operation takes O(1) time.
+type QuickUnion struct {
+	parent []int
+	count  int
+}
+
+// New initializes an empty union-find data structure with n elements from 0
+// through n-1. Initially, each elements is in its own set.
+func New(n int) unionfind.UnionFind {
+	parent := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+	}
+
+	return &QuickUnion{
+		parent: parent,
+		count:  n,
+	}
+}
+
+// Union merges the set containing element p with the the set containing element q.
+func (qu *QuickUnion) Union(p, q int) error {
+	if err := qu.validate(p); err != nil {
+		return err
+	}
+
+	if err := qu.validate(q); err != nil {
+		return err
+	}
+
+	rootP := qu.find(p)
+	rootQ := qu.find(q)
+
+	// p and q are already in the same component
+	if rootP == rootQ {
+		return nil
+	}
+
+	qu.parent[rootP] = rootQ
+	qu.count--
+
+	return nil
+}
+
+// Find returns the canonical element of the set containing element p.
+func (qu *QuickUnion) Find(p int) (int, error) {
+	if err := qu.validate(p); err != nil {
+		return 0, err
+	}
+
+	return qu.find(p), nil
+}
+
+// IsConnected returns true if the two elements are in the same set.
+func (qu *QuickUnion) IsConnected(p, q int) (bool, error) {
+	if err := qu.validate(p); err != nil {
+		return false, err
+	}
+
+	if err := qu.validate(q); err != nil {
+		return false, err
+	}
+
+	rootP := qu.find(p)
+	rootQ := qu.find(q)
+
+	return rootP == rootQ, nil
+}
+
+// Count returns the number of sets.
+func (qu *QuickUnion) Count() int {
+	return qu.count
+}
+
+// validate that p is a valid index.
+func (qu *QuickUnion) validate(p int) error {
+	n := len(qu.parent)
+	if p < 0 || p >= n {
+		return fmt.Errorf("index %d is not between 0 and %d", p, n-1)
+	}
+
+	return nil
+}
+
+// find returns the canonical element of the set containing element p.
+func (qu *QuickUnion) find(p int) int {
+	for p != qu.parent[p] {
+		p = qu.parent[p]
+	}
+
+	return p
+}
---
+++ unionfind/quickunion/quickunion_test.go
@@ -0,0 +1,146 @@
+package quickunion
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnion(t *testing.T) {
+	uf := New(10)
+
+	t.Run("index is less than 0", func(t *testing.T) {
+		err := uf.Union(-1, 0)
+		assert.Equal(t, "index -1 is not between 0 and 9", err.Error())
+	})
+
+	t.Run("index is more or equal to n", func(t *testing.T) {
+		err := uf.Union(0, 10)
+		assert.Equal(t, "index 10 is not between 0 and 9", err.Error())
+	})
+
+	t.Run("index between 0 and n", func(t *testing.T) {
+		assert.Nil(t, uf.Union(0, 9))
+		assert.Nil(t, uf.Union(1, 9))
+		assert.Nil(t, uf.Union(4, 5))
+	})
+
+	t.Run("union twice", func(t *testing.T) {
+		assert.Nil(t, uf.Union(3, 5))
+		assert.Nil(t, uf.Union(3, 5))
+	})
+}
+
+func TestCount(t *testing.T) {
+	uf := New(5)
+
+	// 1 2 3 4 0
+	assert.Equal(t, 5, uf.Count())
+
+	// 1 2 3 4-0
+	assert.Nil(t, uf.Union(0, 4))
+	assert.Equal(t, 4, uf.Count())
+
+	// 1 2-3 4-0
+	assert.Nil(t, uf.Union(2, 3))
+	assert.Equal(t, 3, uf.Count())
+
+	// 1-2-3 4-0
+	assert.Nil(t, uf.Union(1, 2))
+	assert.Equal(t, 2, uf.Count())
+
+	// 1-2-3-4-0
+	assert.Nil(t, uf.Union(4, 3))
+	assert.Equal(t, 1, uf.Count())
+}
+
+func TestIsConnected(t *testing.T) {
+	uf := New(5)
+
+	assert.Nil(t, uf.Union(0, 4))
+	assert.Nil(t, uf.Union(2, 3))
+	assert.Nil(t, uf.Union(1, 2))
+
+	// 1-2-3 4-0
+
+	t.Run("index is less than 0", func(t *testing.T) {
+		isConnected, err := uf.IsConnected(-1, 4)
+		assert.False(t, isConnected)
+		assert.Equal(t, "index -1 is not between 0 and 4", err.Error())
+	})
+
+	t.Run("index is more or equal to n", func(t *testing.T) {
+		isConnected, err := uf.IsConnected(0, 5)
+		assert.False(t, isConnected)
+		assert.Equal(t, "index 5 is not between 0 and 4", err.Error())
+	})
+
+	t.Run("index between 0 and n", func(t *testing.T) {
+		isConnected, err := uf.IsConnected(4, 0)
+		assert.True(t, isConnected)
+		assert.Nil(t, err)
+
+		isConnected, err = uf.IsConnected(1, 3)
+		assert.True(t, isConnected)
+		assert.Nil(t, err)
+
+		isConnected, err = uf.IsConnected(1, 2)
+		assert.True(t, isConnected)
+		assert.Nil(t, err)
+
+		isConnected, err = uf.IsConnected(2, 3)
+		assert.True(t, isConnected)
+		assert.Nil(t, err)
+
+		isConnected, err = uf.IsConnected(3, 4)
+		assert.False(t, isConnected)
+		assert.Nil(t, err)
+
+		isConnected, err = uf.IsConnected(1, 0)
+		assert.False(t, isConnected)
+		assert.Nil(t, err)
+	})
+}
+
+func TestFind(t *testing.T) {
+	uf := New(5)
+
+	t.Run("index is less than 0", func(t *testing.T) {
+		v, err := uf.Find(-1)
+		assert.Equal(t, 0, v)
+		assert.Equal(t, "index -1 is not between 0 and 4", err.Error())
+	})
+
+	t.Run("index is more or equal to n", func(t *testing.T) {
+		v, err := uf.Find(5)
+		assert.Equal(t, 0, v)
+		assert.Equal(t, "index 5 is not between 0 and 4", err.Error())
+	})
+
+	t.Run("index between 0 and n", func(t *testing.T) {
+		assert.Nil(t, uf.Union(0, 4))
+
+		v, err := uf.Find(0)
+		assert.Equal(t, 4, v)
+		assert.Nil(t, err)
+
+		v, err = uf.Find(4)
+		assert.Equal(t, 4, v)
+		assert.Nil(t, err)
+
+		assert.Nil(t, uf.Union(2, 3))
+		assert.Nil(t, uf.Union(1, 2))
+
+		v, err = uf.Find(1)
+		assert.Equal(t, 3, v)
+		assert.Nil(t, err)
+
+		v, err = uf.Find(2)
+		assert.Equal(t, 3, v)
+		assert.Nil(t, err)
+
+		v, err = uf.Find(3)
+		assert.Equal(t, 3, v)
+		assert.Nil(t, err)
+	})
+}
